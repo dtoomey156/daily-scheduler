0c44ec57c669f2998316c9b9ef0fda64e46cd5aa
Daniel Toomey <danieltoomey@Daniels-iMac.lan>
2021-03-22 16:50:49 -0700

    commented out javascript function and implemented Moment.js current date script

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 
+// print current date to CurrentDay id paragraph container
 
+//document.getElementById("currentDay").innerHTML = formatAMPM();
 
-document.getElementById("currentDay").innerHTML = formatAMPM();
+//function formatAMPM() {
+    //var date = new Date();
+    //var hours = date.getHours();
+    //var days = date.getDay();
+    //var minutes = date.getMinutes();
+    //var ampm = hours >=12 ? 'pm' : 'am';
+    //hours = hours %12;
+    //hours = hours ? hours : 12;
+    //minutes = minutes <10 ? '0'+minutes : minutes;
+    //var strTime = date + ' ' + hours + ':' + minutes + ' ' + ampm;
+    //return strTime
+//}
 
-function formatAMPM() {
-    var date = new Date();
-    var hours = date.getHours();
-    var days = date.getDay();
-    var minutes = date.getMinutes();
-    var ampm = hours >=12 ? 'pm' : 'am';
-    hours = hours %12;
-    hours = hours ? hours : 12;
-    minutes = minutes <10 ? '0'+minutes : minutes;
-    var strTime = date + ' ' + hours + ':' + minutes + ' ' + ampm;
-    return strTime
-}
+//commented out javascript date function above. below is Moment.js
+
+$(document).ready(function () {
+    var date = moment().format("dddd MMMM Do");
+    var nowHour = moment().format("HH");
+    $("#currentDay").text(date)
+})
